8b56bed4623475006e469ca35cf1e98a70a3b46f
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-09-28 15:15:05 +0700

    admin

--- routes/adminSetting.js
+++ routes/adminSetting.js
@@ -3,6 +3,13 @@ const data = require('../data/adminSetting');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    try {
+        res.json(data);
+    }
+    catch (err) {console.error(err);}
+});
+
 router.put('/', (req, res) => {
     console.log(data)
   const { isAvailableReserve } = req.body;
