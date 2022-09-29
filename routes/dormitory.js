d9e9fabaa942e82cfaf9dd50b60cb8c8c8e2e282
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-09-29 20:17:58 +0700

    filter cardRoom

--- routes/dormitory.js
+++ routes/dormitory.js
@@ -1,26 +1,59 @@
-const express = require('express');
-const data = require('../data/dormitory');
+const express = require("express");
+const data = require("../data/dormitory");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    try {
-        res.json(data);
-    }
-    catch (err) {console.error(err);}
+router.get("/", (req, res) => {
+  try {
+    const filters = req.query;
+    console.log(filters);
+
+    let room = data;
+    //localhost:8000/dormitories
+    !filters.sex ? null : (room = data.filter((v) => v.id == filters.sex));
+    !filters.floor
+      ? null
+      : (room = room.map((v) => {
+          const filteredRoom = v.rooms
+            .map((v) => {
+              if (v.floor == filters.floor) {
+                return v;
+              }
+            })
+            .filter((v) => v != undefined);
+
+          console.log(filteredRoom);
+          return {
+            ...v,
+            rooms: filteredRoom,
+          };
+        }));
+    // const rooms = data.filter((room) => {
+    //   let isValid = true;
+    //   for()
+    // });
+    console.log({ room: room.map((v) => v.rooms) });
+    res.json(room);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
+  const dormitoryId = Number.parseInt(req.params.id);
+  const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
+  res.json(dormitory);
+});
+
+router.get("/:id", (req, res) => {
   const dormitoryId = Number.parseInt(req.params.id);
   const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
   res.json(dormitory);
 });
 
 let currentDormitoryId = 4;
-router.post('/', (req, res) => {
-  const { 
-    buildingNo,floor, sex, name,tel
-    } = req.body;
+router.post("/", (req, res) => {
+  const { buildingNo, floor, sex, name, tel } = req.body;
   const dormitory = {
     id: ++currentDormitoryId,
     buildingNo,
@@ -28,38 +61,38 @@ router.post('/', (req, res) => {
     sex,
     name,
     tel,
-    rooms:[],
+    rooms: [],
   };
-  
+
   data.push(dormitory);
   res.json(dormitory);
 });
 
-router.put('/:id', (req, res) => {
-    const { 
-        buildingNo,floor, sex, name,tel
-    } = req.body;
-    const dormitoryId = Number.parseInt(req.params.id);
-    const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
+router.put("/:id", (req, res) => {
+  const { buildingNo, floor, sex, name, tel } = req.body;
+  const dormitoryId = Number.parseInt(req.params.id);
+  const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
 
-    dormitory.buildingNo = buildingNo;
-    dormitory.floor = floor;
-    dormitory.sex = sex;
-    dormitory.name = name;
-    dormitory.tel = tel;
+  dormitory.buildingNo = buildingNo;
+  dormitory.floor = floor;
+  dormitory.sex = sex;
+  dormitory.name = name;
+  dormitory.tel = tel;
 
-    res.json(dormitory);
+  res.json(dormitory);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   try {
     const dormitoryId = Number.parseInt(req.params.id);
-    const dormitoryIndex = data.findIndex(dormitory => dormitory.id === dormitoryId);
+    const dormitoryIndex = data.findIndex(
+      (dormitory) => dormitory.id === dormitoryId
+    );
     data.splice(dormitoryIndex, 1);
     res.sendStatus(204);
+  } catch (err) {
+    console.error(err);
   }
-  catch (err) {console.error(err);}
 });
-
 
 module.exports = router;
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,51 +1,46 @@
-const express = require('express');
-const data = require('../data/dormitory');
+const express = require("express");
+const data = require("../data/dormitory");
 
 const router = express.Router();
 
-router.post('/:id', (req, res) => {
+router.post("/:id", (req, res) => {
+  const dormitoryId = Number.parseInt(req.params.id);
+  const dataDormitoryID = data.find(
+    (dormitory) => dormitory.id === dormitoryId
+  );
 
-    const dormitoryId = Number.parseInt(req.params.id); 
-    const dataDormitoryID = data.find((dormitory) => dormitory.id === dormitoryId)
+  let currentRoomId = dataDormitoryID.rooms.length;
 
-    let currentRoomId = dataDormitoryID.rooms.length;
-    
-    const {
-        floor,roomNo,numMax
-    } = req.body;
-    const room = {
-        id: ++currentRoomId,
-        floor,
-        roomNo,
-        numMax,
-        numStudent:0
-    };
+  const { floor, roomNo, numMax } = req.body;
+  const room = {
+    id: ++currentRoomId,
+    floor,
+    roomNo,
+    numMax,
+    numStudent: 0,
+  };
 
-    dataDormitoryID.rooms.push(room);
-    res.json(dataDormitoryID);
+  dataDormitoryID.rooms.push(room);
+  res.json(dataDormitoryID);
 });
 
-router.put('/:id/:idRoom', (req, res) => {
+router.put("/:id/:idRoom", (req, res) => {
+  const { roomNo, numMax } = req.body;
 
-    const { 
-        roomNo,numMax
-    } = req.body;
+  const dormitoryId = Number.parseInt(req.params.id);
+  const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
 
-    const dormitoryId = Number.parseInt(req.params.id);
-    const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
+  const roomId = Number.parseInt(req.params.idRoom);
+  const room = dormitory.rooms.find((room) => room.id === roomId);
 
-    const roomId = Number.parseInt(req.params.idRoom);
-    const room = dormitory.rooms.find((room) => room.id === roomId);
+  room.roomNo = roomNo;
+  room.numMax = numMax;
 
-    room.roomNo = roomNo;
-    room.numMax = numMax;
-
-    res.json(room);
+  res.json(room);
 });
 
-router.delete('/:id/:idRoom', (req, res) => {
+router.delete("/:id/:idRoom", (req, res) => {
   try {
-
     const dormitoryId = Number.parseInt(req.params.id);
     const dormitory = data.find((dormitory) => dormitory.id === dormitoryId);
     const roomId = Number.parseInt(req.params.idRoom);
@@ -53,8 +48,9 @@ router.delete('/:id/:idRoom', (req, res) => {
 
     dormitory.rooms.splice(roomIndex, 1);
     res.sendStatus(204);
+  } catch (err) {
+    console.error(err);
   }
-  catch (err) {console.error(err);}
 });
 
 module.exports = router;
